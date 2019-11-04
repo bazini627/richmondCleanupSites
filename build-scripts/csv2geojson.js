08389358b9b7d4b4345fa204f4cb94aec6cbdecd
U-CDPHINTRA\mbacinsk <mbacinsk@2UA8382PNH.cdphintra.ca.gov>
2019-11-04 11:47:51 -0800

    prettified js scripts

--- build-scripts/csv2geojson.js
+++ build-scripts/csv2geojson.js
@@ -7,7 +7,7 @@ const chalk = require('chalk');
 const csv2Geo = function convertCsvToGeojson() {
 
     // Read in our cleanup site csv file
-    fs.readFile(__dirname + '/../project-files/geoTrackerSites.csv', 'utf-8', (err, csvString) =>{
+    fs.readFile(__dirname + '/../project-files/geoTrackerSites.csv', 'utf-8', (err, csvString) => {
 
         if (err) throw err;
 
@@ -41,19 +41,19 @@ const filterFields = function filterGeojsonFields(geojson) {
 
     // Shorthand for features in geojson
     let features = geojson.features,
-    
-    // Empty array that will hold our filtered features
-    newFeatures = [];
+
+        // Empty array that will hold our filtered features
+        newFeatures = [];
 
     // Loop through the features of our geojson
     features.forEach((feature) => {
-        
+
         // Temporary object to store the fields we wish to keep
         tempProps = {}
-        
+
         // Only keep the features if the city is Richmond and the status is Active or Open...
-        if (feature.properties.CITY === 'RICHMOND' && (feature.properties.STATUS.includes('OPEN') || feature.properties.STATUS === 'ACTIVE' )) {
-            
+        if (feature.properties.CITY === 'RICHMOND' && (feature.properties.STATUS.includes('OPEN') || feature.properties.STATUS === 'ACTIVE')) {
+
             for (let prop in feature.properties) {
                 // Add the properties of our filtered data to the tempProps object
                 tempProps = feature.properties;
