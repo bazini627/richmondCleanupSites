105f6fc816f8a463a8919c477f783fedfa14c7f7
Matt <matthew.bacinskas@uky.edu>
2019-11-04 20:48:38 -0800

    data: changed mapShaperProcess.js script to output Richmond city limit json to wgs84

--- build-scripts/mapshaperProcess.js
+++ build-scripts/mapshaperProcess.js
@@ -4,5 +4,5 @@ const ms = require('mapshaper')
 ms.runCommands('../project-files/richmondNabes.json -filter-fields nhcode,nhname -simplify dp 30% -o precision=.0001 format=geojson ../data/richmondNabes.json');
 
 // Filter out some of the fields the city limits shapefile to only pull out Richmond and simplify the lines
-ms.runCommands("../project-files/contraCostaCityLimits/BND_DCD_City_Limits.shp -filter-fields NAME -filter NAME=='Richmond' -simplify dp 30% -o precision=.0001 format=geojson ../data/richmondCityLimits.json");
+ms.runCommands("../project-files/contraCostaCityLimits/BND_DCD_City_Limits.shp -proj EPSG:4326 -filter-fields NAME -filter NAME=='Richmond' -simplify dp 30% -o precision=.0001 format=geojson ../data/richmondCityLimits.json");
 
