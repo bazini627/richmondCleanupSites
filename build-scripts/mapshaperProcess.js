e90b71d792a1b9c295006d0accefdec1b9ca98a4
Matt <matthew.bacinskas@uky.edu>
2019-11-04 20:43:12 -0800

    data: add line to mapShaperProcess.js to filter out Richmond from the city limit shapefile and output to json

--- build-scripts/mapshaperProcess.js
+++ build-scripts/mapshaperProcess.js
@@ -2,3 +2,7 @@ const ms = require('mapshaper')
 
 // Filter out some of the fields in our richmondaNabes.json file
 ms.runCommands('../project-files/richmondNabes.json -filter-fields nhcode,nhname -simplify dp 30% -o precision=.0001 format=geojson ../data/richmondNabes.json');
+
+// Filter out some of the fields the city limits shapefile to only pull out Richmond and simplify the lines
+ms.runCommands("../project-files/contraCostaCityLimits/BND_DCD_City_Limits.shp -filter-fields NAME -filter NAME=='Richmond' -simplify dp 30% -o precision=.0001 format=geojson ../data/richmondCityLimits.json");
+
