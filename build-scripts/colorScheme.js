29a4e051cbcef55bd9eb47d09343c00a5a3d1e63
U-CDPHINTRA\mbacinsk <mbacinsk@2UA8382PNH.cdphintra.ca.gov>
2019-11-04 16:23:23 -0800

    project: created a color scheme script to extract the pastel theme from the cartocolors json

---
+++ build-scripts/colorScheme.js
@@ -0,0 +1,38 @@
+// Declare all of our required modules
+const fs = require('fs');
+const chalk = require('chalk');
+
+// Function to extrac the pastel color scheme from the cartocolors json
+const getColors = function extractColorFromJson() {
+
+    // Read in the cartocolors json
+    fs.readFile(__dirname + '/../project-files/cartocolors.json', function (err, response) {
+
+        if (err) throw err;
+
+        console.log(chalk.cyan("cartocolors.json file has been loaded"));
+
+        // Assign the parsed JSON data to a variable
+        const colorData = JSON.parse(response);
+
+        console.log(chalk.cyan("cartocolors.json file has been parsed"));
+
+        // Assign the desired key:value pairs from the json to a variable
+        const dataToOutput = {
+            'Pastel': data['Pastel']
+        };
+
+        console.log(chalk.cyan("Pastel color scheme has been extracted from the parsed data"));
+
+        // Output the pastel color scheme to a json file
+        fs.writeFile(__dirname + '/..data/pastelColors.json', JSON.stringify(dataToOutput), 'utf-8', function (err) {
+
+            if (err) throw err;
+
+            console.log(chalk.cyan("pastelColors.json has been created"));
+        });
+    });
+}
+
+// Export the function to later be used as a module
+exports.getColors = getColors;
--- build-scripts/processData.js
+++ build-scripts/processData.js
@@ -1,5 +1,7 @@
 // Declare that we would like to use our csv2geosjon script as a module
 const csvConversion = require('./csv2geojson.js');
+const colorScheme = require('./colorScheme.js');
 
 // Run the module to convert our csv to geojson
 csvConversion.csv2Geo();
+colorScheme.getColors();
